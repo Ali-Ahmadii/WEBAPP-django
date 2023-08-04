d83bc7dd2f5a5f2bcce3c20fe2902519ffc08fef
Ali-Ahmadii <aliahmadi138102@gmail.com>
2023-08-04 18:19:21 +0430

    search

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger , Paginator
+from listings.choices import state_choices, bedroom_choices, price_choices
 # Create your views here.
 def index(request):
     listings = Listing.objects.all()
@@ -18,4 +19,11 @@ def listing(request,listing_id):
     }
     return render(request, 'listings/listing.html',context)
 def search(request):
-    return render(request, 'listings/search.html')
+    listings = Listing.objects.all()
+    context = {
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+    return render(request, 'listings/search.html',context)
