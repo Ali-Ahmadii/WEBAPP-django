bd64b7840cc5d16bab1b7d021b43dea45d3211e9
Ali-Ahmadii <aliahmadi138102@gmail.com>
2023-08-04 22:17:40 +0430

    m

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render , redirect
 
 # Create your views here.
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        return
+    else:
+        return render(request, 'accounts/login.html')
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        #register to db\
+        return
+    else:
+        return render(request, 'accounts/register.html')
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
 def logout(request):
