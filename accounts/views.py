4d80a8c79313731c57a67d7a922696239286854b
Ali-Ahmadii <aliahmadi138102@gmail.com>
2023-08-07 20:43:26 +0430

    registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render , redirect
-from django.contrib import messages
+from django.contrib import messages , auth
+from django.contrib.auth.models import User
 # Create your views here.
 def login(request):
     if request.method == 'POST':
@@ -8,6 +9,25 @@ def login(request):
         return render(request, 'accounts/login.html')
 def register(request):
     if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request,'Username Taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request,'Email Taken')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    user.save()
+                    messages.success(request,'You are now registered and can log in')
+                    return redirect('login')
         messages.error(request,'Something went wrong.')
         return redirect('register')
     else:
