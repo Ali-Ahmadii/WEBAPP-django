cf2e1b66bf44f5186375f71939b6151f05ea7013
Ali-Ahmadii <aliahmadi138102@gmail.com>
2023-08-08 11:47:45 +0430

    fix login and dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render , redirect
 from django.contrib import messages , auth
 from django.contrib.auth.models import User
+from contacts.models import Contact
 # Create your views here.
 def login(request):
-    if request.method == 'POST':
-        return
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+      auth.login(request, user)
+      messages.success(request, 'You are now logged in')
+      return redirect('dashboard')
     else:
-        return render(request, 'accounts/login.html')
+      messages.error(request, 'Invalid credentials')
+      return redirect('login')
+  else:
+    return render(request, 'accounts/login.html')
 def register(request):
     if request.method == 'POST':
         first_name = request.POST['first_name']
@@ -33,7 +45,12 @@ def register(request):
     else:
         return render(request, 'accounts/register.html')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+  user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+
+  context = {
+    'contacts': user_contacts
+  }
+  return render(request, 'accounts/dashboard.html', context)
 def logout(request):
     if request.method == 'POST':
         auth.logout(request)
